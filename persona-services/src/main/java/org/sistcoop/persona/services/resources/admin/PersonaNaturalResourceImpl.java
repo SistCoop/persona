43164042b31fd4194f456073f97c6a35fa58be45
Carlos <carlosthe19916@gmail.com>
2015-06-10 19:07:35 -0500

    google drive

--- persona-services/src/main/java/org/sistcoop/persona/services/resources/admin/PersonaNaturalResourceImpl.java
+++ persona-services/src/main/java/org/sistcoop/persona/services/resources/admin/PersonaNaturalResourceImpl.java
@@ -3,11 +3,8 @@ package org.sistcoop.persona.services.resources.admin;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -36,42 +33,26 @@ import org.sistcoop.persona.models.enums.Sexo;
 import org.sistcoop.persona.models.utils.ModelToRepresentation;
 import org.sistcoop.persona.models.utils.RepresentationToModel;
 import org.sistcoop.persona.representations.idm.PersonaNaturalRepresentation;
-import org.sistcoop.persona.services.util.DriveSample;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.media.MediaHttpUploader;
-import com.google.api.client.http.FileContent;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-import com.google.api.services.drive.model.ParentReference;
-import com.google.api.services.drive.model.Permission;
+import org.sistcoop.persona.services.util.GoogleDriveManager;
 
 @Stateless
 @SecurityDomain("keycloak")
 public class PersonaNaturalResourceImpl implements PersonaNaturalResource {
 
 	@Inject
-	protected PersonaNaturalProvider personaNaturalProvider;
+	private GoogleDriveManager googleDriveManager;
+	
+	@Inject
+	private PersonaNaturalProvider personaNaturalProvider;
 
 	@Inject
-	protected TipoDocumentoProvider tipoDocumentoProvider;
+	private TipoDocumentoProvider tipoDocumentoProvider;
 
 	@Inject
-	protected RepresentationToModel representationToModel;
+	private RepresentationToModel representationToModel;
 
 	@Context
-	protected UriInfo uriInfo;
+	private UriInfo uriInfo;
     
 	@RolesAllowed(Roles.ver_personas)
 	@Override
@@ -165,13 +146,7 @@ public class PersonaNaturalResourceImpl implements PersonaNaturalResource {
 	public void remove(String id) {
 		PersonaNaturalModel personaNaturalModel = personaNaturalProvider.getPersonaNaturalById(id);
 		personaNaturalProvider.removePersonaNatural(personaNaturalModel);
-	}
-
-	JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	HttpTransport httpTransport;
-	FileDataStoreFactory dataStoreFactory;
-	java.io.File DATA_STORE_DIR =
-		      new java.io.File(System.getProperty("user.home"), ".store/drive_sample");
+	}	
 	
 	@RolesAllowed(Roles.administrar_personas)
 	@Override
@@ -193,37 +168,13 @@ public class PersonaNaturalResourceImpl implements PersonaNaturalResource {
 				fos.close();
 				
 				//google drive											
-				try {
-					httpTransport = GoogleNetHttpTransport.newTrustedTransport();	
-					dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-				    Credential credential = authorize();
-					Drive service = new Drive.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(
-					          "sistcoop").build();
-										
-					File body = new File();
-					body.setTitle("/carlos/My document");
-					body.setDescription("A test document");
-					body.setMimeType("image/*");
-					
-					//java.io.File fileContent = new java.io.File("C:\\Users\\Huertas\\document.txt");
-					FileContent mediaContent = new FileContent("text/plain", UPLOAD_FILE);
-					
-					File file = service.files().insert(body, mediaContent).execute();
-					System.out.println("File ID: " + file.getId());
-					
-					//update model					
-				    String url = file.getAlternateLink();
-				    model.setUrlFoto(url);
-				    model.commit();
-				    
-				} catch (GeneralSecurityException e) {					
-					e.printStackTrace();
-				}				
+				String url = googleDriveManager.upload(UPLOAD_FILE, "sistcoop", UUID.randomUUID().toString(), "image/*", "Photo siscoop");														  
+			    model.setUrlFoto(url);
+			    model.commit();				
 			    			    
-			} catch (IOException e) {
-				System.out.println("IOException....................................");
-				System.out.println(e.getMessage());
-				System.out.println(e.getCause().toString());
+			} catch (IOException e) {				
+				throw new InternalServerErrorException();
+			} catch (GeneralSecurityException e) {					
 				throw new InternalServerErrorException();
 			}
 		}		
@@ -233,37 +184,5 @@ public class PersonaNaturalResourceImpl implements PersonaNaturalResource {
 	public void setFirma(String id, MultipartFormDataInput input) {
 		
 	}
-	
-	/**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-	private Credential authorize() {
-		try {
-			// load client secrets
-		    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-		        new InputStreamReader(DriveSample.class.getResourceAsStream("/client_secret.json")));
-		    if (clientSecrets.getDetails().getClientId().startsWith("Enter")
-		        || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
-		      System.out.println(
-		          "Enter Client ID and Secret from https://code.google.com/apis/console/?api=drive "
-		          + "into drive-cmdline-sample/src/main/resources/client_secrets.json");
-		      System.exit(1);
-		    }
-		    // set up authorization code flow
-		    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-		        httpTransport, JSON_FACTORY, clientSecrets,
-		        Collections.singleton(DriveScopes.DRIVE_FILE)).setDataStoreFactory(dataStoreFactory)
-		        .build();
-		    // authorize
-		    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		  
-		} catch (Exception e) {
-			System.out.println("errorrrrrrrrrrrrrrr");
-			System.out.println(e.getMessage());
-		}
-		return null;
-	}   
 
 }
